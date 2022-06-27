bcdd33200fea93c86f2801c24a0683397588303c
puppycan3 <60397942+puppycan3@users.noreply.github.com>
2022-06-27 18:50:46 +0900

    middleware morgan

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  return res.send("this is main page");
-};
-const handleLogin = (req, res) => {
-  return res.send({ message: "this is login page" });
+const home = (req, res) => {
+  console.log("I will respond home");
+  return res.send("hello");
 };
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}/`);
